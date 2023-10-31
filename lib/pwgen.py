aa35386af7342b8d49eb20c29a6f95f4ba46c4ac
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2023-10-31 06:27:41 -0600

    Correccion para usar firebase settings

--- lib/pwgen.py
+++ lib/pwgen.py
@@ -1,22 +1,21 @@
 """
 Generadores de contraseñas
 """
-import os
 import random
 import string
 
-from dotenv import load_dotenv
 from hashids import Hashids
 
-load_dotenv()
-SALT = os.environ.get("SALT", "Esta es una muy mala cadena aleatoria")
+from config.settings import get_settings
+
+settings = get_settings()
 
 
 def generar_api_key(id: int, email: str, random_length: int = 24) -> str:
     """Generar API key a partir de un ID, un e-mail y una cadena aleatoria"""
     aleatorio = "".join(random.sample(string.ascii_letters + string.digits, k=random_length))
     hash_email = Hashids(salt=email, min_length=8).encode(1)
-    hash_id = Hashids(salt=SALT, min_length=8).encode(id)
+    hash_id = Hashids(salt=settings.SALT, min_length=8).encode(id)
     return f"{hash_id}.{hash_email}.{aleatorio}"
 
 
