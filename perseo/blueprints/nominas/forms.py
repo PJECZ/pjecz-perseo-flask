4dc27c52364a96ac49163ab6bbcadde27279ad97
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2023-11-30 10:12:01 -0600

    Nuevos campos en nominas y personas

--- perseo/blueprints/nominas/forms.py
+++ perseo/blueprints/nominas/forms.py
@@ -2,8 +2,8 @@
 Nominas, formularios
 """
 from flask_wtf import FlaskForm
-from wtforms import FloatField, SelectField, StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import DateField, FloatField, SelectField, StringField, SubmitField
+from wtforms.validators import DataRequired, Optional
 
 from perseo.blueprints.nominas.models import Nomina
 
@@ -16,8 +16,10 @@ class NominaEditForm(FlaskForm):
     persona_nombre_completo = StringField("Nombre completo")  # Solo lectura
     centro_trabajo_clave = StringField("Centro de Trabajo")  # Solo lectura
     plaza_clave = StringField("Plaza")  # Solo lectura
+    tipo = SelectField("Tipo", choices=Nomina.TIPOS, validators=[DataRequired()])
     percepcion = FloatField("Percepción", validators=[DataRequired()])
     deduccion = FloatField("Deducción", validators=[DataRequired()])
     importe = FloatField("Importe", validators=[DataRequired()])
-    tipo = SelectField("Tipo", choices=Nomina.TIPOS, validators=[DataRequired()])
+    num_cheque = StringField("Número de cheque", validators=[Optional()])
+    fecha_pago = DateField("Fecha de pago", validators=[Optional()])
     guardar = SubmitField("Guardar")
